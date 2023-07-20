488555dae500c1f5192cd40ea9f1e05913e49bf6
XeloX1UP <108200170+XeloX1UP@users.noreply.github.com>
2023-07-19 23:10:19 -0400

    fix rrssLink component

--- src/components/card/rrssLink.tsx
+++ src/components/card/rrssLink.tsx
@@ -15,10 +15,10 @@ export default function RrSsLink({
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className={`italic shadow-${color} shadow-sm hover:text-amber-400 hover:shadow-amber-200 hover:scale-95 hover:shadow-md rounded-xl px-4 py-2 flex items-center gap-2 text-2xl font-bold transition-all duration-500`}
+      className="italic shadow-slate-600 shadow-sm hover:text-amber-400 hover:shadow-amber-200 hover:scale-95 hover:shadow-md rounded-xl px-4 py-2 flex items-center gap-2 text-2xl font-bold transition-all duration-500"
     >
       {name}
-      <i className={`text-${color}`}>{icon}</i>
+      <i className={`text-white`}>{icon}</i>
     </a>
   );
 }
